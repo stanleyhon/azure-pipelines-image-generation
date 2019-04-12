0849d61f1885e54e1579d60cb5aeed59fd5d2125
Stanley Hon <stahon@microsoft.com>
2019-04-12 11:16:27 -0700

    Adding scripts to installing Edge and Validating Edge installed.

---
+++ images/win/scripts/Installers/Validate-EdgePreview.ps1
@@ -0,0 +1,16 @@
+################################################################################
+##  File:  Validate-EdgePreview.ps1
+##  Team:  Automated Testing
+##  Desc:  Validate Edge Preview installation.
+################################################################################
+
+$edgePath = Join-Path ${env:programfiles(x86)} "Microsoft\Edge Dev\Application\msedge.exe"
+if(Test-Path $edgePath)
+{
+    exit 0
+}
+else
+{
+    Write-Host "Microsoft Edge Preview is not installed."
+    exit 1
+} 
